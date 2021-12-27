2bec61551a98053b4710eea175e5f63841f2a183
Omid Heidarzadeh <arashmidos1370@yahoo.com>
2021-12-27 10:59:42 +0330

    change port config to lowercase

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require('./startup/config')();
 require('./startup/db')();
 require('./startup/routes')(app);
 
-const port = process.env.PORT || config.get('PORT') || 3000;
+const port = process.env.PORT || config.get('port') || 3000;
 const server = app.listen(port, () => winston.info(`Listening to port ${port}...`));
 
 module.exports = server;
